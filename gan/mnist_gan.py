2bf08a7b75b2706280f2beb0cd4e04353b152c26
sharavsambuu gunchinish <sharavsambuu@gmail.com>
2018-04-16 19:54:58 +0800

    trying to implement vanilla GAN

--- gan/mnist_gan.py
+++ gan/mnist_gan.py
@@ -1,7 +1,111 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.gridspec as gridspec
+
+plt.rcParams['figure.figsize']      = (10.0, 8.0)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap']          = 'gray'
 
 """
 GAN, Generative Adversarial Network
 MNIST шиг харагддаг зурагнуудыг үүсгэж сургах.
 """
+
+def generator(z, output_dim, reuse=False, alpha=0.2, training=True):
+    """
+    Defines the generator network
+    :param z: input random vector z
+    :param output_dim: output dimension of the network
+    :param reuse: Indicates whether or not the existing model variables should be used or recreated
+    :param alpha: scalar for lrelu activation function
+    :param training: Boolean for controlling the batch normalization statistics
+    :return: model's output
+    """
+    with tf.variable_scope('generator', reuse=reuse):
+        fc1 = dense(z, 4*4*512)
+
+        # Reshape it to start the convolutional stack
+        fc1 = tf.reshape(fc1, (-1, 4, 4, 512))
+        fc1 = batch_norm(fc1, training=training)
+        fc1 = tf.nn.relu(fc1)
+
+        t_conv1 = transpose_conv2d(fc1, 256)
+        t_conv1 = batch_norm(t_conv1, training=training)
+        t_conv1 = tf.nn.relu(t_conv1)
+
+        t_conv2 = transpose_conv2d(t_conv1, 128)
+        t_conv2 = batch_norm(t_conv2, training=training)
+        t_conv2 = tf.nn.relu(t_conv2)
+
+        logits = transpose_conv2d(t_conv2, output_dim)
+
+        out = tf.tanh(logits)
+        return out
+
+def discriminator(x, reuse=False, alpha=0.2, training=True):
+    """
+    Defines the discriminator network
+    :param x: input for network
+    :param reuse: Indicates whether or not the existing model variables should be used or recreated
+    :param alpha: scalar for lrelu activation function
+    :param training: Boolean for controlling the batch normalization statistics
+    :return: A tuple of (sigmoid probabilities, logits)
+    """
+    with tf.variable_scope('discriminator', reuse=reuse):
+        # Input layer is 32x32x?
+        conv1 = conv2d(x, 64)
+        conv1 = lrelu(conv1, alpha)
+
+        conv2 = conv2d(conv1, 128)
+        conv2 = batch_norm(conv2, training=training)
+        conv2 = lrelu(conv2, alpha)
+
+        conv3 = conv2d(conv2, 256)
+        conv3 = batch_norm(conv3, training=training)
+        conv3 = lrelu(conv3, alpha)
+
+        # Flatten it
+        flat = tf.reshape(conv3, (-1, 4*4*256))
+        logits = dense(flat, 1)
+
+        out = tf.sigmoid(logits)
+        return out, logits
+
+def model_loss(input_real, input_z, output_dim, alpha=0.2, smooth=0.1):
+    """
+    Get the loss for the discriminator and generator
+    :param input_real: Images from the real dataset
+    :param input_z: random vector z
+    :param out_channel_dim: The number of channels in the output image
+    :param smooth: label smothing scalar
+    :return: A tuple of (discriminator loss, generator loss)
+    """
+    g_model = generator(input_z, output_dim, alpha=alpha)
+    d_model_real, d_logits_real = discriminator(input_real, alpha=alpha)
+
+    d_model_fake, d_logits_fake = discriminator(g_model, reuse=True, alpha=alpha)
+
+    # for the real images, we want them to be classified as positives,  
+    # so we want their labels to be all ones.
+    # notice here we use label smoothing for helping the discriminator to generalize better.
+    # Label smoothing works by avoiding the classifier to make extreme predictions when extrapolating.
+    d_loss_real = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(
+        	logits=d_logits_real, labels=tf.ones_like(d_logits_real) * (1 - smooth)))
+
+    # for the fake images produced by the generator, we want the discriminator to clissify 
+    # them as false images, so we set their labels to be all zeros.
+    d_loss_fake = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(
+        	logits=d_logits_fake, labels=tf.zeros_like(d_model_fake)))
+
+    # since the generator wants the discriminator to output 1s for its images, it uses the 
+    # discriminator logits for the fake images and assign labels of 1s to them.
+    g_loss = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(
+        	logits=d_logits_fake, labels=tf.ones_like(d_model_fake)))
+
+    d_loss = d_loss_real + d_loss_fake
+
+    return d_loss, g_loss
